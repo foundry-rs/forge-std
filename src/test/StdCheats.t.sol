040e6ad6b19bf75d9a8cc3d88e37adccd83bfd20
Jack Scobell <scobelljack@gmail.com>
2022-05-01 21:28:29 -0400

    modified tests to work with new bound algo from commit 0b4285b2ae9792b2efb599a8cb4650ecd42b6a70

--- src/test/StdCheats.t.sol
+++ src/test/StdCheats.t.sol
@@ -80,13 +80,13 @@ contract StdCheatsTest is Test {
         assertEq(bound(5, 0, 4), 0);
         assertEq(bound(0, 69, 69), 69);
         assertEq(bound(0, 68, 69), 68);
-        assertEq(bound(10, 150, 190), 174);
-        assertEq(bound(300, 2800, 3200), 3107);
-        assertEq(bound(9999, 1337, 6666), 4669);
+        assertEq(bound(10, 150, 190), 160);
+        assertEq(bound(300, 2800, 3200), 3100);
+        assertEq(bound(9999, 1337, 6666), 6006);
         
     }
 
-    function testRevertBoundMinBiggerThanMax() public {
+    function testCannotBoundMaxLessThanMin() public {
         vm.expectRevert(bytes("MAX_LESS_THAN_MIN"));
         bound(5, 100, 10);
     }
@@ -105,10 +105,10 @@ contract StdCheatsTest is Test {
     }
 
     function testBoundUint256Max() public {
-        assertEq(bound(0, type(uint256).max - 1, type(uint256).max), type(uint256).max);
+        assertEq(bound(0, type(uint256).max - 1, type(uint256).max), type(uint256).max - 1);
     }
 
-    function testRevertBoundMinBiggerThanMax(
+    function testCannotBoundMaxLessThanMin(
         uint256 num,
         uint256 min,
         uint256 max
