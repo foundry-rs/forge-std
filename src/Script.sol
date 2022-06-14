edf820f31f3eeee3246bdace67c7e5343ba2f32f
Matt Solomon <matt@mattsolomon.dev>
2022-06-14 11:57:16 -0700

    feat: add IS_SCRIPT to know if a contract is a script

--- src/Script.sol
+++ src/Script.sol
@@ -6,6 +6,7 @@ import "./console.sol";
 import "./console2.sol";
 
 abstract contract Script {
+    bool public IS_SCRIPT = true;
     address constant private VM_ADDRESS =
         address(bytes20(uint160(uint256(keccak256('hevm cheat code')))));
 
