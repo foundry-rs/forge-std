f32dbdc3f014176539d95099cbe617968fd0b059
Paul Razvan Berg <hello@paulrberg.com>
2022-07-14 00:12:57 +0300

    Optimize same sign check (#114)
    * Optimize same sign check
    
    * Add comment about why "a^b>-1" works

--- src/Test.sol
+++ src/Test.sol
@@ -710,7 +710,8 @@ library stdMath {
 
     function delta(int256 a, int256 b) internal pure returns (uint256) {
         // a and b are of the same sign
-        if (a >= 0 && b >= 0 || a < 0 && b < 0) {
+        // this works thanks to two's complement, the left-most bit is the sign bit
+        if ((a ^ b) > -1) {
             return delta(abs(a), abs(b));
         }
 
