1ed67912c2457491ab705aefc2982f05b35a2203
Paul Razvan Berg <hello@paulrberg.com>
2022-07-18 20:07:23 +0300

    test: move "testGenerateCorrectAddress" to "StdUtils.t.sol"

--- src/test/StdUtils.t.sol
+++ src/test/StdUtils.t.sol
@@ -46,4 +46,9 @@ contract StdUtilsTest is Test {
         vm.expectRevert(bytes("Test bound(uint256,uint256,uint256): Max is less than min."));
         bound(num, min, max);
     }
+
+    function testGenerateCorrectAddress() external {
+        address creation = computeCreateAddress(0x6C9FC64A53c1b71FB3f9Af64d1ae3A4931A5f4E9, 14);
+        assertEq(creation, 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45);
+    }
 }
