2508f4724de659af75bf48e8c425e097d199a7dd
Paul Razvan Berg <hello@paulrberg.com>
2022-07-21 13:54:01 +0300

    refactor: define "vm" and "stdStorage" in "TestBase"
    feat: add "Components.sol" file which re-exports everything

--- src/Components.sol
+++ src/Components.sol
@@ -3,12 +3,9 @@ pragma solidity >=0.6.0 <0.9.0;
 
 import "./Assertions.sol";
 import "./Cheats.sol";
-import "./console.sol";
-import "./console2.sol";
 import "./Errors.sol";
 import "./Math.sol";
 import "./Storage.sol";
 import "./TestBase.sol";
 import "./Utils.sol";
-
-abstract contract Test is TestBase, Assertions, Cheats, Utils {}
+import "./Vm.sol";
