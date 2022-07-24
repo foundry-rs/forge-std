167b34cdbcc3efbd7eee42280ff23b0c48932553
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-07-24 19:19:14 +0200

    ✨ (Components) Export consoles

--- src/Components.sol
+++ src/Components.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.6.0 <0.9.0;
 
 import "./Assertions.sol";
 import "./Cheats.sol";
+import "./console.sol";
+import "./console2.sol";
 import "./Errors.sol";
 import "./Math.sol";
 import "./Storage.sol";
