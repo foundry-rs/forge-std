f53e030706fbc5b94332756fe46a569d6348115d
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-07-24 20:11:52 +0200

    ♻️ Import from Components

--- src/Script.sol
+++ src/Script.sol
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
-import "./Cheats.sol";
-import "./console.sol";
-import "./console2.sol";
-import "./Utils.sol";
-import "./Vm.sol";
+import {Cheats, console, console2, stdMath, stdStorage, StdStorage, Utils, Vm} from "./Components.sol";
 
 abstract contract ScriptBase {
     address internal constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
--- src/Test.sol
+++ src/Test.sol
@@ -1,16 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
+import "./Components.sol";
 import "ds-test/test.sol";
-import "./Assertions.sol";
-import "./Cheats.sol";
-import "./console.sol";
-import "./console2.sol";
-import "./Errors.sol";
-import "./Math.sol";
-import "./Storage.sol";
-import "./Utils.sol";
-import "./Vm.sol";
 
 abstract contract TestBase {
     address internal constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
