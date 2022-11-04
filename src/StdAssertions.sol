22c773d412f22f55c7116d9d9ea49488a99bdba4
Matt Solomon <matt@mattsolomon.dev>
2022-11-04 10:15:14 -0700

    build: no relative paths for ds-test, to better support node_modules uses

--- src/StdAssertions.sol
+++ src/StdAssertions.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2 <0.9.0;
 
-import "../lib/ds-test/src/test.sol";
+import "ds-test/test.sol";
 import "./StdMath.sol";
 
 abstract contract StdAssertions is DSTest {
