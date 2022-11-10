2c442a61545ea7e2258ed1d29f89c7ff4e1af33c
Matt Solomon <matt@mattsolomon.dev>
2022-11-09 19:06:25 -0800

    ci: add a test contract to compile Test in CI

---
+++ src/test/Compilation.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2 <0.9.0;
+
+import "../Test.sol";
+
+// Even though this contract is in the `src/` directory, it is a test contract. It simply inherits
+// from Test, which is abstract, to test compilation in CI across versions, in particular with
+// via-ir. We cannot compile Test directly because it is abstract, and we don't place this in the
+// `test/` directory because otherwise we need to compile that full directory, which increases CI
+// time, especially with via-ir.
+contract CompilationTest is Test {}
