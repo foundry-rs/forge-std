3d17201138abd303ee0175cf2d7bc0bd6710419c
James Wenzel <wenzel.james.r@gmail.com>
2022-11-19 06:50:46 -0800

    rename interface (#223)
    Co-authored-by: James Wenzel <jameswenzel@users.noreply.github.com>

--- src/interfaces/IERC1155.sol
+++ src/interfaces/IERC1155.sol
@@ -5,7 +5,7 @@ import "./IERC165.sol";
 /// @title ERC-1155 Multi Token Standard
 /// @dev See https://eips.ethereum.org/EIPS/eip-1155
 /// Note: The ERC-165 identifier for this interface is 0xd9b67a26.
-interface ERC1155 is IERC165 {
+interface IERC1155 is IERC165 {
     /// @dev
     /// - Either `TransferSingle` or `TransferBatch` MUST emit when tokens are transferred, including zero value transfers as well as minting or burning (see "Safe Transfer Rules" section of the standard).
     /// - The `_operator` argument MUST be the address of an account/contract that is approved to make the transfer (SHOULD be msg.sender).
