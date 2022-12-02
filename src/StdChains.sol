00d76d6356aeb7e61067072b1b121f51418e3170
Adrien Husson <adhusson@gmail.com>
2022-12-02 17:00:03 +0100

    feat: improve StdChains

--- src/StdChains.sol
+++ src/StdChains.sol
@@ -8,6 +8,11 @@ import "./Vm.sol";
 abstract contract StdChains {
     VmSafe private constant vm = VmSafe(address(uint160(uint256(keccak256("hevm cheat code")))));
 
+    /// @dev To hide constructor warnings across solc versions due to different constructor visibility requirements and
+    /// syntaxes, we put the constructor in a private method and assign an unused return value to a variable. This
+    /// forces the method to run during construction, but without declaring an explicit constructor.
+    uint256 private CONSTRUCTOR = _constructor();
+
     struct Chain {
         // The chain name.
         string name;
@@ -20,71 +25,128 @@ abstract contract StdChains {
         string rpcUrl;
     }
 
-    bool private initialized;
+    // chain alias -> chain data
+    mapping(string => Chain) internal chains;
+    mapping(string => string) internal defaultRpcUrls;
+    mapping(uint256 => string) private idToAlias;
 
-    // Maps from a chain's key (matching the alias in the `foundry.toml` file) to chain data.
-    mapping(string => Chain) private chains;
-    // Maps from a chain ID to that chain's key name
-    mapping(uint256 => string) private idToKey;
-
-    function getChain(string memory key) internal virtual returns (Chain memory) {
-        initialize();
-        return chains[key];
-    }
-
-    function getChain(uint256 chainId) internal virtual returns (Chain memory) {
-        initialize();
-        return chains[idToKey[chainId]];
-    }
-
-    function setChain(string memory key, string memory name, uint256 chainId, string memory rpcUrl) internal virtual {
+    // The RPC URL will be fetched from config or defaultRpcUrls if possible.
+    function getChain(string memory chainAlias) public view returns (Chain memory chain) {
+        chain = chains[chainAlias];
         require(
-            keccak256(bytes(idToKey[chainId])) == keccak256(bytes(""))
-                || keccak256(bytes(idToKey[chainId])) == keccak256(bytes(key)),
+            chain.chainId != 0,
+            string(abi.encodePacked("StdChains getChain(string): Chain alias \"", chainAlias, "\" not found."))
+        );
+
+        withRpcUrl(chainAlias, chain);
+    }
+
+    function getChain(uint256 chainId) internal virtual returns (Chain memory chain) {
+        string memory chainAlias = idToAlias[chainId];
+
+        require(
+            bytes(idToAlias[chainId]).length != 0,
+            string(
+                abi.encodePacked("StdChains getChain(uint): No alias found for chain ID ", vm.toString(chainId), ".")
+            )
+        );
+
+        chain = chains[chainAlias];
+
+        require(
+            chain.chainId != 0,
+            string(abi.encodePacked("StdChains getChain(uint): Chain ID ", vm.toString(chainId), " not found."))
+        );
+
+        withRpcUrl(chainAlias, chain);
+    }
+
+    // lookup rpcUrl, in descending order of priority:
+    // current -> config (foundry.toml) -> default
+    function withRpcUrl(string memory chainAlias, Chain memory chain) private view {
+        if (bytes(chain.rpcUrl).length == 0) {
+            try vm.rpcUrl(chainAlias) returns (string memory configRpcUrl) {
+                chain.rpcUrl = configRpcUrl;
+            } catch (bytes memory err) {
+                chain.rpcUrl = defaultRpcUrls[chainAlias];
+                // distinguish 'not found' from 'cannot read'
+                bytes memory notFoundError =
+                    abi.encodeWithSignature("CheatCodeError", string(abi.encodePacked("invalid rpc url ", chainAlias)));
+                if (keccak256(notFoundError) != keccak256(err) || bytes(chain.rpcUrl).length == 0) {
+                    /// @solidity memory-safe-assembly
+                    assembly {
+                        revert(add(32, err), mload(err))
+                    }
+                }
+            }
+        }
+    }
+
+    // set chain info, with priority to chainAlias' rpc url in foundry.toml
+    function setChainWithDefaultRpcUrl(string memory chainAlias, Chain memory chain) internal {
+        string memory rpcUrl = chain.rpcUrl;
+        defaultRpcUrls[chainAlias] = rpcUrl;
+        chain.rpcUrl = "";
+        setChain(chainAlias, chain);
+        chain.rpcUrl = rpcUrl; // restore argument
+    }
+
+    // set chain info, with priority to argument's rpcUrl field.
+    function setChain(string memory chainAlias, Chain memory chain) internal virtual {
+        require(
+            bytes(chainAlias).length != 0, "StdChains setChain(string,Chain): chainAlias cannot be the empty string."
+        );
+
+        require(chain.chainId != 0, "StdChains setChain(string,Chain): Chain ID 0 cannot be used.");
+
+        string memory foundAlias = idToAlias[chain.chainId];
+
+        require(
+            bytes(foundAlias).length == 0 || keccak256(bytes(foundAlias)) == keccak256(bytes(chainAlias)),
             string(
                 abi.encodePacked(
-                    "StdChains setChain(string,string,uint256,string): Chain ID ",
-                    vm.toString(chainId),
+                    "StdChains setChain(string,Chain): Chain ID ",
+                    vm.toString(chain.chainId),
                     " already used by \"",
-                    idToKey[chainId],
+                    foundAlias,
                     "\"."
                 )
             )
         );
 
-        uint256 oldChainId = chains[key].chainId;
-        delete idToKey[oldChainId];
+        uint256 oldChainId = chains[chainAlias].chainId;
+        delete idToAlias[oldChainId];
 
-        chains[key] = Chain(name, chainId, rpcUrl);
-        idToKey[chainId] = key;
+        chains[chainAlias] = chain;
+        idToAlias[chain.chainId] = chainAlias;
     }
 
-    function initialize() private {
-        if (initialized) return;
+    function _constructor() private returns (uint256) {
+        setChainWithDefaultRpcUrl("anvil", Chain("Anvil", 31337, "http://127.0.0.1:8545"));
+        setChainWithDefaultRpcUrl(
+            "mainnet", Chain("Mainnet", 1, "https://mainnet.infura.io/v3/6770454bc6ea42c58aac12978531b93f")
+        );
+        setChainWithDefaultRpcUrl(
+            "goerli", Chain("Goerli", 5, "https://goerli.infura.io/v3/6770454bc6ea42c58aac12978531b93f")
+        );
+        setChainWithDefaultRpcUrl("sepolia", Chain("Sepolia", 11155111, "https://rpc.sepolia.dev"));
+        setChainWithDefaultRpcUrl("optimism", Chain("Optimism", 10, "https://mainnet.optimism.io"));
+        setChainWithDefaultRpcUrl("optimism_goerli", Chain("Optimism Goerli", 420, "https://goerli.optimism.io"));
+        setChainWithDefaultRpcUrl("arbitrum_one", Chain("Arbitrum One", 42161, "https://arb1.arbitrum.io/rpc"));
+        setChainWithDefaultRpcUrl(
+            "arbitrum_one_goerli", Chain("Arbitrum One Goerli", 421613, "https://goerli-rollup.arbitrum.io/rpc")
+        );
+        setChainWithDefaultRpcUrl("arbitrum_nova", Chain("Arbitrum Nova", 42170, "https://nova.arbitrum.io/rpc"));
+        setChainWithDefaultRpcUrl("polygon", Chain("Polygon", 137, "https://polygon-rpc.com"));
+        setChainWithDefaultRpcUrl("polygon_mumbai", Chain("Polygon Mumbai", 80001, "https://rpc-mumbai.matic.today"));
+        setChainWithDefaultRpcUrl("avalanche", Chain("Avalanche", 43114, "https://api.avax.network/ext/bc/C/rpc"));
+        setChainWithDefaultRpcUrl(
+            "avalanche_fuji", Chain("Avalanche Fuji", 43113, "https://api.avax-test.network/ext/bc/C/rpc")
+        );
+        setChainWithDefaultRpcUrl("bnb_smart_chain", Chain("BNB Smart Chain", 56, "https://bsc-dataseed1.binance.org"));
+        setChainWithDefaultRpcUrl("bnb_smart_chain_testnet", Chain("BNB Smart Chain Testnet", 97, "https://data-seed-prebsc-1-s1.binance.org:8545"));// forgefmt: disable-line
+        setChainWithDefaultRpcUrl("gnosis_chain", Chain("Gnosis Chain", 100, "https://rpc.gnosischain.com"));
 
-        setChain("anvil", "Anvil", 31337, "http://127.0.0.1:8545");
-        setChain("mainnet", "Mainnet", 1, "https://mainnet.infura.io/v3/6770454bc6ea42c58aac12978531b93f");
-        setChain("goerli", "Goerli", 5, "https://goerli.infura.io/v3/6770454bc6ea42c58aac12978531b93f");
-        setChain("sepolia", "Sepolia", 11155111, "https://rpc.sepolia.dev");
-        setChain("optimism", "Optimism", 10, "https://mainnet.optimism.io");
-        setChain("optimism_goerli", "Optimism Goerli", 420, "https://goerli.optimism.io");
-        setChain("arbitrum_one", "Arbitrum One", 42161, "https://arb1.arbitrum.io/rpc");
-        setChain("arbitrum_one_goerli", "Arbitrum One Goerli", 421613, "https://goerli-rollup.arbitrum.io/rpc");
-        setChain("arbitrum_nova", "Arbitrum Nova", 42170, "https://nova.arbitrum.io/rpc");
-        setChain("polygon", "Polygon", 137, "https://polygon-rpc.com");
-        setChain("polygon_mumbai", "Polygon Mumbai", 80001, "https://rpc-mumbai.matic.today");
-        setChain("avalanche", "Avalanche", 43114, "https://api.avax.network/ext/bc/C/rpc");
-        setChain("avalanche_fuji", "Avalanche Fuji", 43113, "https://api.avax-test.network/ext/bc/C/rpc");
-        setChain("bnb_smart_chain", "BNB Smart Chain", 56, "https://bsc-dataseed1.binance.org");
-        setChain("bnb_smart_chain_testnet", "BNB Smart Chain Testnet", 97, "https://data-seed-prebsc-1-s1.binance.org:8545");// forgefmt: disable-line
-        setChain("gnosis_chain", "Gnosis Chain", 100, "https://rpc.gnosischain.com");
-
-        // Loop over RPC URLs in the config file to replace the default RPC URLs
-        Vm.Rpc[] memory rpcs = vm.rpcUrlStructs();
-        for (uint256 i = 0; i < rpcs.length; i++) {
-            chains[rpcs[i].key].rpcUrl = rpcs[i].url;
-        }
-
-        initialized = true;
+        return 0;
     }
 }
--- test/StdChains.t.sol
+++ test/StdChains.t.sol
@@ -17,25 +17,38 @@ contract StdChainsTest is Test {
 
     // Ensure we can connect to the default RPC URL for each chain.
     function testRpcs() public {
-        (string[2][] memory rpcs) = vm.rpcUrls();
-        for (uint256 i = 0; i < rpcs.length; i++) {
-            ( /* string memory name */ , string memory rpcUrl) = (rpcs[i][0], rpcs[i][1]);
-            vm.createSelectFork(rpcUrl);
-        }
+        string memory rpcUrl;
+        rpcUrl = vm.rpcUrl("mainnet");
+        vm.createSelectFork(rpcUrl);
+        rpcUrl = vm.rpcUrl("optimism_goerli");
+        vm.createSelectFork(rpcUrl);
+        rpcUrl = vm.rpcUrl("arbitrum_one_goerli");
+        vm.createSelectFork(rpcUrl);
+    }
+
+    function testChainNoDefault() public {
+        vm.expectRevert("StdChains getChain(string): Chain alias \"does_not_exist\" not found.");
+        getChain("does_not_exist");
+    }
+
+    function testChainBubbleUp() public {
+        setChain("needs_undefined_env_var", Chain("", 123456789, ""));
+        vm.expectRevert(
+            "Failed to resolve env var `UNDEFINED_RPC_URL_PLACEHOLDER` in `${UNDEFINED_RPC_URL_PLACEHOLDER}`: environment variable not found"
+        );
+        getChain("needs_undefined_env_var");
     }
 
     function testCannotSetChain_ChainIdExists() public {
-        setChain({key: "custom_chain", name: "Custom Chain", chainId: 123456789, rpcUrl: "https://custom.chain/"});
+        setChain("custom_chain", Chain("Custom Chain", 123456789, "https://custom.chain/"));
 
-        vm.expectRevert(
-            'StdChains setChain(string,string,uint256,string): Chain ID 123456789 already used by "custom_chain".'
-        );
+        vm.expectRevert('StdChains setChain(string,Chain): Chain ID 123456789 already used by "custom_chain".');
 
-        setChain({key: "another_custom_chain", name: "", chainId: 123456789, rpcUrl: ""});
+        setChain("another_custom_chain", Chain("", 123456789, ""));
     }
 
     function testSetChain() public {
-        setChain({key: "custom_chain", name: "Custom Chain", chainId: 123456789, rpcUrl: "https://custom.chain/"});
+        setChain("custom_chain", Chain("Custom Chain", 123456789, "https://custom.chain/"));
         Chain memory customChain = getChain("custom_chain");
         assertEq(customChain.name, "Custom Chain");
         assertEq(customChain.chainId, 123456789);
@@ -46,12 +59,33 @@ contract StdChainsTest is Test {
         assertEq(chainById.rpcUrl, customChain.rpcUrl);
     }
 
+    function testNoChainId0() public {
+        vm.expectRevert("StdChains setChain(string,Chain): chainAlias cannot be the empty string.");
+        setChain("", Chain("", 123456789, ""));
+    }
+
+    function testNoEmptyAlias() public {
+        vm.expectRevert("StdChains setChain(string,Chain): Chain ID 0 cannot be used.");
+        setChain("alias", Chain("", 0, ""));
+    }
+
+    function testChainIdNotFound() public {
+        vm.expectRevert("StdChains getChain(string): Chain alias \"no_such_alias\" not found.");
+        getChain("no_such_alias");
+    }
+
+    function testChainAliasNotFound() public {
+        vm.expectRevert("StdChains getChain(uint): No alias found for chain ID 321.");
+        getChain(321);
+    }
+
     function testSetChain_ExistingOne() public {
-        setChain({key: "custom_chain", name: "Custom Chain", chainId: 123456789, rpcUrl: "https://custom.chain/"});
+        setChain("custom_chain", Chain("Custom Chain", 123456789, "https://custom.chain/"));
         assertEq(getChain(123456789).chainId, 123456789);
 
-        setChain({key: "custom_chain", name: "Modified Chain", chainId: 999999999, rpcUrl: "https://modified.chain/"});
-        assertEq(getChain(123456789).chainId, 0);
+        setChain("custom_chain", Chain("Modified Chain", 999999999, "https://modified.chain/"));
+        vm.expectRevert("StdChains getChain(uint): No alias found for chain ID 123456789.");
+        getChain(123456789);
 
         Chain memory modifiedChain = getChain(999999999);
         assertEq(modifiedChain.name, "Modified Chain");
