4b9b197494f301c5d4028454102158259ef53ff9
Matt Solomon <matt@mattsolomon.dev>
2022-12-15 07:36:44 -0800

    docs: add comments explaining how StdChains works and how to use it

--- src/StdChains.sol
+++ src/StdChains.sol
@@ -5,6 +5,35 @@ pragma experimental ABIEncoderV2;
 
 import {VmSafe} from "./Vm.sol";
 
+/**
+ * StdChains provides information about EVM compatible chains that can be used in scripts/tests.
+ * For each chain, the chain's name, chain ID, and a default RPC URL are provided. Chains are
+ * identified by their alias, which is the same as the alias in the `[rpc_endpoints]` section of
+ * the `foundry.toml` file. For best UX, ensure the alias in the `foundry.toml` file match the
+ * alias used in this contract, which can be found as the first argument to the
+ * `setChainWithDefaultRpcUrl` call in the `initialize` function.
+ *
+ * There are two main ways to use this contract:
+ *   1. Set a chain with `setChain(string memory chainAlias, Chain memory chain)`
+ *   2. Get a chain with `getChain(string memory chainAlias)` or `getChain(uint256 chainId)`.
+ *
+ * The first time either of those are used, chains are initialized with the default set of RPC URLs.
+ * This is done in `initialize`, which uses `setChainWithDefaultRpcUrl`. Defaults are recorded in
+ * `defaultRpcUrls`.
+ *
+ * The `setChain` function is straightforward, and it simply saves off the given chain data.
+ *
+ * The `getChain` methods use `withRpcUrl` to return a chain. For example, let's say we want to
+ * retrieve `mainnet`'s RPC URL:
+ *   - If you haven't set any mainnet chain info with `setChain` and you haven't specified that
+ *     chain in `foundry.toml`, the default data and RPC URL will be returned.
+ *   - If you have set a mainnet RPC URL in `foundry.toml` it will return that, if valid (e.g. if
+ *     a URL is given or if an environment variable is given and that environment variable exists).
+ *     Otherwise, the default data is returned.
+ *   - If you specified data with `setChain` it will return that.
+ *
+ * Summarizing the above, the prioritization hierarchy is `setChain` -> `foundry.toml` -> defaults.
+ */
 abstract contract StdChains {
     VmSafe private constant vm = VmSafe(address(uint160(uint256(keccak256("hevm cheat code")))));
 
