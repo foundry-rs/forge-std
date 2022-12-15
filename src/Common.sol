5b2757de9ebd6461c5ece654d3cfdd9c172efc6b
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-12-15 11:46:14 +0100

    fix: put constants back

--- src/Common.sol
+++ src/Common.sol
@@ -11,6 +11,12 @@ abstract contract CommonBase {
     // console.sol and console2.sol work by executing a staticcall to this address.
     address internal constant CONSOLE = 0x000000000000000000636F6e736F6c652e6c6f67;
 
+    // Default address for tx.origin and msg.sender, 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38.
+    address internal constant DEFAULT_SENDER = address(uint160(uint256(keccak256("foundry default caller"))));
+
+    // Address of the test contract, deployed by the DEFAULT_SENDER.
+    address internal constant DEFAULT_TEST_CONTRACT = 0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f;
+
     uint256 internal constant UINT256_MAX =
         115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
--- src/Test.sol
+++ src/Test.sol
@@ -15,12 +15,6 @@ import {StdStorage, stdStorage} from "./StdStorage.sol";
 import {StdUtils} from "./StdUtils.sol";
 import {Vm} from "./Vm.sol";
 
-abstract contract TestBase is CommonBase {
-    // Default address for tx.origin and msg.sender, 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38.
-    address internal constant DEFAULT_SENDER = address(uint160(uint256(keccak256("foundry default caller"))));
-
-    // Address of the test contract, deployed by the DEFAULT_SENDER.
-    address internal constant DEFAULT_TEST_CONTRACT = 0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f;
-}
+abstract contract TestBase is CommonBase {}
 
 abstract contract Test is DSTest, StdAssertions, StdChains, StdCheats, StdUtils, TestBase {}
