001505927969494bcbd99309e5e65a66ac95e601
Matt Solomon <matt@mattsolomon.dev>
2023-01-17 13:37:17 -0800

    fix: backwards compatibility with older solc versions

--- src/StdUtils.sol
+++ src/StdUtils.sol
@@ -122,7 +122,11 @@ abstract contract StdUtils {
         virtual
         returns (uint256[] memory balances)
     {
-        require(token.code.length > 0, "StdUtils getTokenBalances(address,address[]): Token address is not a contract.");
+        uint256 tokenCodeSize;
+        assembly {
+            tokenCodeSize := extcodesize(tokenCodeSize)
+        }
+        require(tokenCodeSize > 0, "StdUtils getTokenBalances(address,address[]): Token address is not a contract.");
 
         // ABI encode the aggregate call to Multicall3.
         uint256 length = addresses.length;
