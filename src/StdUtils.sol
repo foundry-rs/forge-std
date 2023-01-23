578968243529db44acffcb802196ccab9b54db88
karmacoma <karma@coma.lol>
2023-01-22 19:50:42 -0500

    Add create2 helper methods (#276)
    * Add create2 helper methods
    
    - computeCreate2Address(bytes32 salt, bytes32 initCodeHash) returns an address using the default create2 deployer
    - hashInitCode(bytes, bytes) and hashInitCodeNoConstructorArgs(bytes) make it more convenient to figure out the init code hash in deploy scripts
    
    * Make create2 helper methods more consistent

--- src/StdUtils.sol
+++ src/StdUtils.sol
@@ -20,6 +20,9 @@ abstract contract StdUtils {
     uint256 private constant UINT256_MAX =
         115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
+    // Used by default when deploying with create2, https://github.com/Arachnid/deterministic-deployment-proxy.
+    address private constant CREATE2_FACTORY = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+
     /*//////////////////////////////////////////////////////////////////////////
                                  INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
@@ -114,6 +117,24 @@ abstract contract StdUtils {
         returns (address)
     {
         return addressFromLast20Bytes(keccak256(abi.encodePacked(bytes1(0xff), deployer, salt, initcodeHash)));
+    }
+
+    /// @dev returns the address of a contract created with CREATE2 using the default CREATE2 deployer
+    function computeCreate2Address(bytes32 salt, bytes32 initCodeHash) internal pure returns (address) {
+        return computeCreate2Address(salt, initCodeHash, CREATE2_FACTORY);
+    }
+
+    /// @dev returns the hash of the init code (creation code + no args) used in CREATE2 with no constructor arguments
+    /// @param creationCode the creation code of a contract C, as returned by type(C).creationCode
+    function hashInitCode(bytes memory creationCode) internal pure returns (bytes32) {
+        return hashInitCode(creationCode, "");
+    }
+
+    /// @dev returns the hash of the init code (creation code + ABI-encoded args) used in CREATE2
+    /// @param creationCode the creation code of a contract C, as returned by type(C).creationCode
+    /// @param args the ABI-encoded arguments to the constructor of C
+    function hashInitCode(bytes memory creationCode, bytes memory args) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(creationCode, args));
     }
 
     // Performs a single call with Multicall3 to query the ERC-20 token balances of the given addresses.
