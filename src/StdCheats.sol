131edf89118b5faa51f59307d37dbd734b17d097
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-04-17 17:17:39 +0300

    feat: add StdTypes
    refactor: move cheats structs to StdTypes

--- src/StdCheats.sol
+++ src/StdCheats.sol
@@ -4,194 +4,25 @@ pragma solidity >=0.6.2 <0.9.0;
 pragma experimental ABIEncoderV2;
 
 import {StdStorage, stdStorage} from "./StdStorage.sol";
+import {
+    Account,
+    EIP1559ScriptArtifact,
+    RawEIP1559ScriptArtifact,
+    RawReceipt,
+    RawReceiptLog,
+    RawTx1559,
+    RawTx1559Detail,
+    Receipt,
+    ReceiptLog,
+    Tx1559,
+    Tx1559Detail
+} from "./StdTypes.sol";
 import {Vm} from "./Vm.sol";
 
 abstract contract StdCheatsSafe {
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
     bool private gasMeteringOff;
-
-    // Data structures to parse Transaction objects from the broadcast artifact
-    // that conform to EIP1559. The Raw structs is what is parsed from the JSON
-    // and then converted to the one that is used by the user for better UX.
-
-    struct RawTx1559 {
-        string[] arguments;
-        address contractAddress;
-        string contractName;
-        // json value name = function
-        string functionSig;
-        bytes32 hash;
-        // json value name = tx
-        RawTx1559Detail txDetail;
-        // json value name = type
-        string opcode;
-    }
-
-    struct RawTx1559Detail {
-        AccessList[] accessList;
-        bytes data;
-        address from;
-        bytes gas;
-        bytes nonce;
-        address to;
-        bytes txType;
-        bytes value;
-    }
-
-    struct Tx1559 {
-        string[] arguments;
-        address contractAddress;
-        string contractName;
-        string functionSig;
-        bytes32 hash;
-        Tx1559Detail txDetail;
-        string opcode;
-    }
-
-    struct Tx1559Detail {
-        AccessList[] accessList;
-        bytes data;
-        address from;
-        uint256 gas;
-        uint256 nonce;
-        address to;
-        uint256 txType;
-        uint256 value;
-    }
-
-    // Data structures to parse Transaction objects from the broadcast artifact
-    // that DO NOT conform to EIP1559. The Raw structs is what is parsed from the JSON
-    // and then converted to the one that is used by the user for better UX.
-
-    struct TxLegacy {
-        string[] arguments;
-        address contractAddress;
-        string contractName;
-        string functionSig;
-        string hash;
-        string opcode;
-        TxDetailLegacy transaction;
-    }
-
-    struct TxDetailLegacy {
-        AccessList[] accessList;
-        uint256 chainId;
-        bytes data;
-        address from;
-        uint256 gas;
-        uint256 gasPrice;
-        bytes32 hash;
-        uint256 nonce;
-        bytes1 opcode;
-        bytes32 r;
-        bytes32 s;
-        uint256 txType;
-        address to;
-        uint8 v;
-        uint256 value;
-    }
-
-    struct AccessList {
-        address accessAddress;
-        bytes32[] storageKeys;
-    }
-
-    // Data structures to parse Receipt objects from the broadcast artifact.
-    // The Raw structs is what is parsed from the JSON
-    // and then converted to the one that is used by the user for better UX.
-
-    struct RawReceipt {
-        bytes32 blockHash;
-        bytes blockNumber;
-        address contractAddress;
-        bytes cumulativeGasUsed;
-        bytes effectiveGasPrice;
-        address from;
-        bytes gasUsed;
-        RawReceiptLog[] logs;
-        bytes logsBloom;
-        bytes status;
-        address to;
-        bytes32 transactionHash;
-        bytes transactionIndex;
-    }
-
-    struct Receipt {
-        bytes32 blockHash;
-        uint256 blockNumber;
-        address contractAddress;
-        uint256 cumulativeGasUsed;
-        uint256 effectiveGasPrice;
-        address from;
-        uint256 gasUsed;
-        ReceiptLog[] logs;
-        bytes logsBloom;
-        uint256 status;
-        address to;
-        bytes32 transactionHash;
-        uint256 transactionIndex;
-    }
-
-    // Data structures to parse the entire broadcast artifact, assuming the
-    // transactions conform to EIP1559.
-
-    struct EIP1559ScriptArtifact {
-        string[] libraries;
-        string path;
-        string[] pending;
-        Receipt[] receipts;
-        uint256 timestamp;
-        Tx1559[] transactions;
-        TxReturn[] txReturns;
-    }
-
-    struct RawEIP1559ScriptArtifact {
-        string[] libraries;
-        string path;
-        string[] pending;
-        RawReceipt[] receipts;
-        TxReturn[] txReturns;
-        uint256 timestamp;
-        RawTx1559[] transactions;
-    }
-
-    struct RawReceiptLog {
-        // json value = address
-        address logAddress;
-        bytes32 blockHash;
-        bytes blockNumber;
-        bytes data;
-        bytes logIndex;
-        bool removed;
-        bytes32[] topics;
-        bytes32 transactionHash;
-        bytes transactionIndex;
-        bytes transactionLogIndex;
-    }
-
-    struct ReceiptLog {
-        // json value = address
-        address logAddress;
-        bytes32 blockHash;
-        uint256 blockNumber;
-        bytes data;
-        uint256 logIndex;
-        bytes32[] topics;
-        uint256 transactionIndex;
-        uint256 transactionLogIndex;
-        bool removed;
-    }
-
-    struct TxReturn {
-        string internalType;
-        string value;
-    }
-
-    struct Account {
-        address addr;
-        uint256 key;
-    }
 
     function assumeNoPrecompiles(address addr) internal virtual {
         // Assembly required since `block.chainid` was introduced in 0.8.0.
