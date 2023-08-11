cea88c1905336f38bfdce99fbf0703f48ba0a899
ashhanai <ashhab.naim@gmail.com>
2023-08-11 19:14:25 +0200

    Fix assumeNotForgeAddress in StdCheats.sol

--- src/StdCheats.sol
+++ src/StdCheats.sol
@@ -342,7 +342,7 @@ abstract contract StdCheatsSafe {
 
     function assumeNotForgeAddress(address addr) internal pure virtual {
         // vm and console addresses
-        vm.assume(addr != address(vm) || addr != 0x000000000000000000636F6e736F6c652e6c6f67);
+        vm.assume(addr != address(vm) && addr != 0x000000000000000000636F6e736F6c652e6c6f67);
     }
 
     function readEIP1559ScriptArtifact(string memory path)
