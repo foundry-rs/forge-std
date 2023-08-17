4772599a9134a7cf24a0561dad2889f6f270bc82
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2023-08-17 11:32:20 -0500

    chore(`vm`): add createWallet() for Wallet struct (#435)
    * chore(vm): add createWallet for Wallet struct, add sign and getNonce for Wallet
    
    * style(vm): add named inputs/outputs for wallet functions
    
    * fix: name conflict on older solc versions
    
    ---------
    
    Co-authored-by: Matt Solomon <matt@mattsolomon.dev>

--- src/Vm.sol
+++ src/Vm.sol
@@ -47,6 +47,24 @@ interface VmSafe {
         uint256 created;
     }
 
+    struct Wallet {
+        address addr;
+        uint256 publicKeyX;
+        uint256 publicKeyY;
+        uint256 privateKey;
+    }
+
+    // Derives a private key from the name, labels the account with that name, and returns the wallet
+    function createWallet(string calldata walletLabel) external returns (Wallet memory wallet);
+    // Generates a wallet from the private key and returns the wallet
+    function createWallet(uint256 privateKey) external returns (Wallet memory wallet);
+    // Generates a wallet from the private key, labels the account with that name, and returns the wallet
+    function createWallet(uint256 privateKey, string calldata walletLabel) external returns (Wallet memory wallet);
+    // Signs data, (Wallet, digest) => (v, r, s)
+    function sign(Wallet calldata wallet, bytes32 digest) external returns (uint8 v, bytes32 r, bytes32 s);
+    // Get nonce for a Wallet
+    function getNonce(Wallet calldata wallet) external returns (uint64 nonce);
+
     // Loads a storage slot from an address
     function load(address target, bytes32 slot) external view returns (bytes32 data);
     // Signs data
