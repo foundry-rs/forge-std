36f27c116993f6bf418195640096f0c84314f0f5
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-01-17 17:42:04 +0100

    fix: don't hardcode string values in Vm generator, adds missing cheats to `Vm.sol` (#497)
    * fix: don't hardcode string values in Vm generator
    
    * chore: update interfaceId

--- test/Vm.t.sol
+++ test/Vm.t.sol
@@ -9,7 +9,7 @@ contract VmTest is Test {
     // inadvertently moved between Vm and VmSafe. This test must be updated each time a function is
     // added to or removed from Vm or VmSafe.
     function test_interfaceId() public {
-        assertEq(type(VmSafe).interfaceId, bytes4(0x7e017c39), "VmSafe");
+        assertEq(type(VmSafe).interfaceId, bytes4(0x90569756), "VmSafe");
         assertEq(type(Vm).interfaceId, bytes4(0xd6a02054), "Vm");
     }
 }
