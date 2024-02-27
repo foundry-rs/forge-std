2bca5fd53f262472b9012ef30c4f82827ad9d268
Maksym Pyvovarov <119981087+maxpv026@users.noreply.github.com>
2024-02-27 23:53:21 +0200

    Update StdMath.sol
    Added SPDX license identifier comment.
    Added comments in English explaining the purpose of each function and the significance of constants.
    Improved clarity of variable names and comments to enhance readability.

--- src/StdMath.sol
+++ src/StdMath.sol
@@ -2,10 +2,12 @@
 pragma solidity >=0.6.2 <0.9.0;
 
 library stdMath {
+    // Minimum value of int256
     int256 private constant INT256_MIN = -57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
+    // Returns the absolute value of an int256 number
     function abs(int256 a) internal pure returns (uint256) {
-        // Required or it will fail when `a = type(int256).min`
+        // Required for handling INT256_MIN
         if (a == INT256_MIN) {
             return 57896044618658097711785492504343953926634992332820282019728792003956564819968;
         }
@@ -13,31 +15,37 @@ library stdMath {
         return uint256(a > 0 ? a : -a);
     }
 
+    // Returns the absolute difference between two uint256 numbers
     function delta(uint256 a, uint256 b) internal pure returns (uint256) {
         return a > b ? a - b : b - a;
     }
 
+    // Returns the absolute difference between two int256 numbers
     function delta(int256 a, int256 b) internal pure returns (uint256) {
-        // a and b are of the same sign
-        // this works thanks to two's complement, the left-most bit is the sign bit
+        // If a and b have the same sign, return the absolute difference
         if ((a ^ b) > -1) {
             return delta(abs(a), abs(b));
         }
 
-        // a and b are of opposite signs
+        // If a and b have opposite signs, return the sum of their absolute values
         return abs(a) + abs(b);
     }
 
+    // Returns the percentage difference between two uint256 numbers
     function percentDelta(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 absDelta = delta(a, b);
 
+        // Calculate the percentage difference
         return absDelta * 1e18 / b;
     }
 
+    // Returns the percentage difference between two int256 numbers
     function percentDelta(int256 a, int256 b) internal pure returns (uint256) {
         uint256 absDelta = delta(a, b);
         uint256 absB = abs(b);
 
+        // Calculate the percentage difference
         return absDelta * 1e18 / absB;
     }
 }
+
