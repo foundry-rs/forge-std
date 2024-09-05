7defada94c4ac8471aa587a21717f28a8a108c9c
milosdjurica <milosdjurica13@gmail.com>
2024-09-05 19:16:16 +0200

    named imports in test/StdToml.t.sol

--- test/StdToml.t.sol
+++ test/StdToml.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-import "../src/Test.sol";
+import {Test, stdToml} from "../src/Test.sol";
 
 contract StdTomlTest is Test {
     using stdToml for string;
