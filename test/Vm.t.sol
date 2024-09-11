e04104ab93e771441eab03fb76eda1402cb5927b
grandizzy <38490174+grandizzy@users.noreply.github.com>
2024-09-11 18:53:08 +0300

    feat: add `setArbitraryStorage`, `copyStorage`, `mockFunction` and `assumeNoRevert` cheatcodes (#602)

--- test/Vm.t.sol
+++ test/Vm.t.sol
@@ -9,7 +9,7 @@ contract VmTest is Test {
     // inadvertently moved between Vm and VmSafe. This test must be updated each time a function is
     // added to or removed from Vm or VmSafe.
     function test_interfaceId() public pure {
-        assertEq(type(VmSafe).interfaceId, bytes4(0x9dd1a1c8), "VmSafe");
-        assertEq(type(Vm).interfaceId, bytes4(0x02edefa2), "Vm");
+        assertEq(type(VmSafe).interfaceId, bytes4(0x74d42398), "VmSafe");
+        assertEq(type(Vm).interfaceId, bytes4(0xaf15a283), "Vm");
     }
 }
