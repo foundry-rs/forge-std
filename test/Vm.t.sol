eb18cf234f8f35e51e3f84b407098607aeae1e10
zerosnacks <zerosnacks@protonmail.com>
2024-10-03 10:09:02 +0200

    add snapshotGas* cheatcodes

--- test/Vm.t.sol
+++ test/Vm.t.sol
@@ -9,7 +9,7 @@ import {Vm, VmSafe} from "../src/Vm.sol";
 // added to or removed from Vm or VmSafe.
 contract VmTest is Test {
     function test_VmInterfaceId() public pure {
-        assertEq(type(Vm).interfaceId, bytes4(0xa561dbe8), "Vm");
+        assertEq(type(Vm).interfaceId, bytes4(0x35457718), "Vm");
     }
 
     function test_VmSafeInterfaceId() public pure {
