42ce013a251372011b6f289ea69e58f3b6e33d15
Yotam Bar-On <tudmotu@gmail.com>
2024-10-04 11:23:14 +0300

    Update Vm with `mockCalls` and VmSafe with `randomBytes` methods

--- test/Vm.t.sol
+++ test/Vm.t.sol
@@ -9,10 +9,10 @@ import {Vm, VmSafe} from "../src/Vm.sol";
 // added to or removed from Vm or VmSafe.
 contract VmTest is Test {
     function test_VmInterfaceId() public pure {
-        assertEq(type(Vm).interfaceId, bytes4(0x35457718), "Vm");
+        assertEq(type(Vm).interfaceId, bytes4(0x61a5f010), "Vm");
     }
 
     function test_VmSafeInterfaceId() public pure {
-        assertEq(type(VmSafe).interfaceId, bytes4(0x681d10d4), "VmSafe");
+        assertEq(type(VmSafe).interfaceId, bytes4(0xf7f67508), "VmSafe");
     }
 }
