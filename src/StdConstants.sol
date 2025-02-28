87989d2f1bd1142be444674eae61437947a16ad6
Igor Żuk <igor.zuk@protonmail.com>
2025-02-28 16:10:41 +0100

    feat: Add StdConstants library (#654)
    Fixes https://github.com/foundry-rs/forge-std/issues/267. Gathers up all
    constants used throughout forge-std in a single library so they are
    easier to access.
    
    ~~The refactoring around the constants is minimal, it's mostly
    replacements of arbitrary values with references to the new library. No
    changes are visible from the outside, the 3 constants removed from
    `StdUtil` were private. Many more refactorings, like dropping other
    private constants that are used more than once or are variations about
    `type(uintXXX).max`, may be done in separate PRs.~~

--- src/StdConstants.sol
+++ src/StdConstants.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2 <0.9.0;
 
-import {StdStorage} from "./StdStorage.sol";
-import {Vm, VmSafe} from "./Vm.sol";
+import {IMulticall3} from "./interfaces/IMulticall3.sol";
+import {Vm} from "./Vm.sol";
 
-abstract contract CommonBase {
+library StdConstants {
     /// @dev Cheat code address.
     /// Calculated as `address(uint160(uint256(keccak256("hevm cheat code"))))`.
-    address internal constant VM_ADDRESS = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+    Vm internal constant VM = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
     /// @dev console.sol and console2.sol work by executing a staticcall to this address.
     /// Calculated as `address(uint160(uint88(bytes11("console.log"))))`.
     address internal constant CONSOLE = 0x000000000000000000636F6e736F6c652e6c6f67;
@@ -23,20 +23,8 @@ abstract contract CommonBase {
     address internal constant DEFAULT_TEST_CONTRACT = 0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f;
     /// @dev Deterministic deployment address of the Multicall3 contract.
     /// Taken from https://www.multicall3.com.
-    address internal constant MULTICALL3_ADDRESS = 0xcA11bde05977b3631167028862bE2a173976CA11;
+    IMulticall3 internal constant MULTICALL3_ADDRESS = IMulticall3(0xcA11bde05977b3631167028862bE2a173976CA11);
     /// @dev The order of the secp256k1 curve.
     uint256 internal constant SECP256K1_ORDER =
         115792089237316195423570985008687907852837564279074904382605163141518161494337;
-
-    uint256 internal constant UINT256_MAX =
-        115792089237316195423570985008687907853269984665640564039457584007913129639935;
-
-    Vm internal constant vm = Vm(VM_ADDRESS);
-    StdStorage internal stdstore;
-}
-
-abstract contract TestBase is CommonBase {}
-
-abstract contract ScriptBase is CommonBase {
-    VmSafe internal constant vmSafe = VmSafe(VM_ADDRESS);
 }
