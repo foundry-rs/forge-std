e2cb00ff21e398d9d364e107235ad2e95b9a662c
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-06-11 11:32:00 +0200

    style: run forge fmt

--- src/Vm.sol
+++ src/Vm.sol
@@ -1940,7 +1940,10 @@ interface VmSafe {
     // Params:
     //  * `bindingsPath`: path where the output of `forge bind-json` is stored.
     //  * `typeName`: Name of the type (i.e. "Transaction").
-    function eip712HashType(string calldata bindingsPath, string calldata typeName) external pure returns (bytes32 typeHash);
+    function eip712HashType(string calldata bindingsPath, string calldata typeName)
+        external
+        pure
+        returns (bytes32 typeHash);
 
     // Generates the struct hash of the canonical EIP-712 type representation and its abi-encoded data.
     //
@@ -1952,7 +1955,10 @@ interface VmSafe {
     //  2. String representation of the type (i.e. "Foo(Bar bar) Bar(uint256 baz)").
     //     * Note: the cheatcode will use the canonical type even if the input is malformated
     //             with the wrong order of elements or with extra whitespaces.
-    function eip712HashStruct(string calldata typeNameOrDefinition, bytes calldata abiEncodedData) external pure returns (bytes32 typeHash);
+    function eip712HashStruct(string calldata typeNameOrDefinition, bytes calldata abiEncodedData)
+        external
+        pure
+        returns (bytes32 typeHash);
 
     // Generates the struct hash of the canonical EIP-712 type representation and its abi-encoded data.
     // Requires previous binding generation with `forge bind-json`.
@@ -1961,7 +1967,10 @@ interface VmSafe {
     //  * `bindingsPath`: path where the output of `forge bind-json` is stored.
     //  * `typeName`: Name of the type (i.e. "PermitSingle").
     //  * `abiEncodedData`: ABI-encoded data for the struct that is being hashed.
-    function eip712HashStruct(string calldata bindingsPath, string calldata typeName, bytes calldata abiEncodedData) external pure returns (bytes32 typeHash);
+    function eip712HashStruct(string calldata bindingsPath, string calldata typeName, bytes calldata abiEncodedData)
+        external
+        pure
+        returns (bytes32 typeHash);
 
     // Generates a ready-to-sign digest of human-readable typed data following the EIP-712 standard.
     function eip712HashTypedData(string calldata jsonData) external pure returns (bytes32 digest);
