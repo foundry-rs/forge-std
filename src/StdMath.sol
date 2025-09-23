0654ae493e5537323181a2ac9097e5a5ae8c079d
viktorking7 <140458814+viktorking7@users.noreply.github.com>
2025-09-23 13:52:08 +0200

    Update StdMath.sol

--- src/StdMath.sol
+++ src/StdMath.sol
@@ -29,6 +29,8 @@ library stdMath {
     }
 
     function percentDelta(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Prevent division by zero
+        require(b != 0, "stdMath: percentDelta divisor is zero");
         uint256 absDelta = delta(a, b);
 
         return absDelta * 1e18 / b;
@@ -37,6 +39,8 @@ library stdMath {
     function percentDelta(int256 a, int256 b) internal pure returns (uint256) {
         uint256 absDelta = delta(a, b);
         uint256 absB = abs(b);
+        // Prevent division by zero
+        require(absB != 0, "stdMath: percentDelta divisor is zero");
 
         return absDelta * 1e18 / absB;
     }
