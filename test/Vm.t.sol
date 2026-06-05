8e848ee33d7eeeae1aa44017f3cd1e1997bf5272
Mablr <59505383+mablr@users.noreply.github.com>
2026-06-05 11:16:15 +0200

    chore: update Vm interface cheatcodes

--- test/Vm.t.sol
+++ test/Vm.t.sol
@@ -9,10 +9,10 @@ import {Vm, VmSafe} from "../src/Vm.sol";
 // added to or removed from Vm or VmSafe.
 contract VmTest is Test {
     function test_VmInterfaceId() public pure {
-        assertEq(type(Vm).interfaceId, bytes4(0x6eba6eb5), "Vm");
+        assertEq(type(Vm).interfaceId, bytes4(0x65c5c1b6), "Vm");
     }
 
     function test_VmSafeInterfaceId() public pure {
-        assertEq(type(VmSafe).interfaceId, bytes4(0xcb532963), "VmSafe");
+        assertEq(type(VmSafe).interfaceId, bytes4(0xad2713c6), "VmSafe");
     }
 }
