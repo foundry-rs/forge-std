7fdf81f9ceb2f6ebbb8f9f1c6c5274d5bcc9a1f5
Matthias Seitz <matthias.seitz@outlook.de>
2026-09-10 13:59:24 -0400

    feat(vm): add slot number cheatcodes (#911)

--- test/Vm.t.sol
+++ test/Vm.t.sol
@@ -9,10 +9,10 @@ import {Vm, VmSafe} from "../src/Vm.sol";
 // added to or removed from Vm or VmSafe.
 contract VmTest is Test {
     function test_VmInterfaceId() public pure {
-        assertEq(type(Vm).interfaceId, bytes4(0x6fde05ab), "Vm");
+        assertEq(type(Vm).interfaceId, bytes4(0x9ae1b86f), "Vm");
     }
 
     function test_VmSafeInterfaceId() public pure {
-        assertEq(type(VmSafe).interfaceId, bytes4(0x9b107c4f), "VmSafe");
+        assertEq(type(VmSafe).interfaceId, bytes4(0xeb03f324), "VmSafe");
     }
 }
